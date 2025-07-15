cf4cb8d8e25f716b6bd4bfaf8af7e2961da996a4
Deepak <thefinancialcraft@gmail.com>
2025-07-15 21:43:19 +0000

    up insight

--- src/pages/Services.tsx
+++ src/pages/Services.tsx
@@ -1,6 +1,7 @@
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 import { Badge } from '@/components/ui/badge';
 import { Button } from '@/components/ui/button';
+import { Analytics } from "@vercel/analytics/react"
 import Footer from '@/components/Footer';
 import { 
   Heart, 
@@ -304,6 +305,7 @@ const Services = () => {
           </div>
         </div>
       </section>
+      <Analytics />       
       {/* Footer */}
       <Footer />
     </div>
