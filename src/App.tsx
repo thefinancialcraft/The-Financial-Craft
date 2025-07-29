692626a3371b1fb42dc72c2186a436dab9efd5e3
Deepak <thefinancialcraft@gmail.com>
2025-07-30 00:39:37 +0530

    ok

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { SpeedInsights } from "@vercel/speed-insights/react"
-import Navbar from "./components/Navbar";
 import Index from "./pages/Index";
 import About from "./pages/About";
 import Services from "./pages/Services";
@@ -12,7 +11,7 @@ import Companies from "./pages/Companies";
 import Career from "./pages/Career";
 import Team from "./pages/Team";
 import NotFound from "./pages/NotFound";
-import Layout from "@/components/layout";
+import Layout from "@/pages/Layout";
 
 // hii
 const queryClient = new QueryClient();
