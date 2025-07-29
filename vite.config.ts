323d373b9a7796560d61296d9025eef0eb246030
Deepak <thefinancialcraft@gmail.com>
2025-07-29 19:30:34 +0530

    ok

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,23 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
+import tailwindPostcss from "@tailwindcss/postcss";
+import autoprefixer from "autoprefixer";
 
-
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
   },
-  plugins: [
-    react(),
-    ],
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  css: {
+    postcss: {
+      plugins: [tailwindPostcss(), autoprefixer()],
     },
   },
 }));
